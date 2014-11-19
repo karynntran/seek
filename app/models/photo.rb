f35af9927526ebc2df32883dce43691c48d1375c
karynntran <karynn.tran@gmail.com>
2014-11-19 18:18:10 -0500

    Update photos methods to take in options for search

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,22 +1,24 @@
 class Photo < ActiveRecord::Base
 
-def get_api_urls
-  api = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8701c7d581eab20399c7d2b7599918ff&lat=48.8567&lon=2.3508&format=rest")
+def self.get_api_urls(options)
+  latitude = options[:latitude]
+  longitude = options[:longitude]
+  api = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8701c7d581eab20399c7d2b7599918ff&lat=#{latitude}&lon=#{longitude}&format=rest")
 
-  all_paris_photos = api.first[1]["photos"]["photo"]
+  all_location_photos = api.first[1]["photos"]["photo"]
 
-  all_paris_ids = all_paris_photos.map do |photo|
+  all_photo_ids = all_location_photos.map do |photo|
     photo["id"]
   end
 
-  all_paris_urls = all_paris_ids.map do |photo_id|
+  all_photo_urls = all_photo_ids.map do |photo_id|
 
     url_call = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=8701c7d581eab20399c7d2b7599918ff&photo_id=#{photo_id}&format=rest")
 
     url_call["rsp"]["sizes"]["size"][4]["source"]
   end
 
-  imgs = all_paris_urls.each do |url|
+  imgs = all_photo_urls.each do |url|
     "<img width='100px' height='auto' src='#{url}'>"
   end
 end
