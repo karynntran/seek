462297737880e1d1014ff395a2b5190ad56d4722
mjprude <michaelprude@gmail.com>
2014-11-20 12:16:10 -0500

    Refactor Photo module into single api call sorted by interestingness

--- lib/modules/photo.rb
+++ lib/modules/photo.rb
@@ -1,37 +1,43 @@
 module Photo
 
   def self.get_photos(options)
-    lat_long = City.find_by(name: options[:destination])
-    url_hash = {}
-    url_hash[:latitutde] = lat_long.split(',')[0]
-    url_hash[:longitude] = lat_long.split(',')[1]
-    get_api_ids(url_hash)
-  end
+    lat_long = City.find_by(name: options[:destination]).lat_long
 
-  def self.get_api_ids(options)
-    latitude = options[:latitude]
-    longitude = options[:longitude]
-    api = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8701c7d581eab20399c7d2b7599918ff&lat=#{latitude}&lon=#{longitude}&format=rest")
-    get_api_ids(api)
-  end
+    api = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=63cc42384b112fe6bd9597dd5e25cc0f&lat=#{lat_long.split(',')[0]}&lon=#{lat_long.split(',')[1]}&format=rest&safe_search=1&extras=url_c&sort=interestingness-desc")
 
-  def self.get_api_urls(api)
-    all_location_photos = api["rsp"]["photos"]["photo"]
+    photo_array = api['rsp']['photos']['photo'].map do |photo|
+      photo['url_c'] ? photo['url_c'] : nil
+    end.compact
 
-    all_photo_ids = all_location_photos.map do |photo|
-      photo["id"]
-    end
-
-    all_photo_urls = all_photo_ids.map do |photo_id|
-
-      url_call = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=8701c7d581eab20399c7d2b7599918ff&photo_id=#{photo_id}&format=rest")
-
-      url_call["rsp"]["sizes"]["size"][4]["source"]
-    end
-
-    imgs = all_photo_urls.each do |url|
-      "<img width='100px' height='auto' src='#{url}'>"
+    photo_array.map do |url|
+      "<img width='100px' height='auto' class='destination_photos' src='#{url}'>"
     end
   end
-
 end
+
+
+
+#     binding.pry
+#     get_api_urls(api)
+#   end
+
+#   def self.get_api_urls(api)
+#     all_location_photos = api["rsp"]["photos"]["photo"]
+
+#     all_photo_ids = all_location_photos.map do |photo|
+#       photo["id"]
+#     end
+#     binding.pry
+#     all_photo_urls = all_photo_ids.map do |photo_id|
+
+#       url_call = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=63cc42384b112fe6bd9597dd5e25cc0f&photo_id=#{photo_id}&format=rest")
+
+#       url_call["rsp"]["sizes"]["size"][4]["source"]
+#     end
+#     binding.pry
+#     imgs = all_photo_urls.each do |url|
+#       "<img width='100px' height='auto' src='#{url}'>"
+#     end
+#   end
+
+# end
