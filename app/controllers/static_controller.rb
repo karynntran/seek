7eaef3936e07d409ace3c560fca0e824a31963cd
CarsonBills <CarsonMBills@Gmail.com>
2014-11-20 10:19:05 -0500

    Flight Info appears on show page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,9 +1,13 @@
 class StaticController < ApplicationController
+  include
   def index
   end
 
   def show
-    binding.pry
-    origin = params[:origin]
+    @origin = params[:origin]
+    @destination = params[:destination]
+    @month = params[:month]
+    hash = {destination: @destination, origin: @origin, month: @month}
+    @average_flight_price= Hotwire.get_flight_info(hash)
   end
 end
