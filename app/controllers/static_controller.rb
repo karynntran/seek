e9b3235c8b98920e841a39e1d3c1eea138e241b1
karynntran <karynn.tran@gmail.com>
2014-11-22 13:38:14 -0500

    Add bm_index variable in controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -17,10 +17,12 @@ class StaticController < ApplicationController
   end
 
   def show
+    binding.pry
     @origin = params[:origin]
     @destination = params[:destination]
     @month = params[:month]
     hash = {destination: @destination, origin: @origin, month: @month}
+    @bm_index = City.where(name: @destination)[0].bm_index
     hotwire_hash = Hotwire.get_flight_info(hash)
     @avg_price = hotwire_hash[:avg_price].to_s
     @max_temp = hotwire_hash[:max_temp].to_s
@@ -28,6 +30,7 @@ class StaticController < ApplicationController
     @avg_precipitation = hotwire_hash[:precipitation].to_s
     @photos = Photo.get_photos(params)
     render :layout => 'show'
+
   end
 
   def photos
