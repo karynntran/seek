2b9f3942d3679554680c91cf5189aeab7b7ecc72
karynntran <karynn.tran@gmail.com>
2014-11-23 00:49:30 -0500

    Change made to controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,7 @@
 class FavoritesController < ApplicationController
 
   def check_favorites
+    binding.pry
     respond_to do |format|
       if current_user && current_user.favorites && current_user.favorites.include?(params[:url])
         format.json { render json: { status: "true" } }
@@ -11,9 +12,7 @@ class FavoritesController < ApplicationController
   end
 
   def add_favorites
-
     favorite = Favorite.create({user_id: current_user.id, link: params[:favorite]})
     current_user.favorites << favorite
   end
-
 end
